d6cd22072ab1e4ba0de3653e8982d3cd1cee4677
ABU SAID <abusaid@Abu-said17.local>
2024-12-05 00:37:07 +0600

    complete crud operation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require("jsonwebtoken")
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -10,6 +11,20 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
+//verify token , token save localstorage
+const verifyToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.szzkw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,33 +42,98 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
+    // use for add json data on backend
     const infoCollection = client.db('RestaurantDB').collection('DBInfo');
 
+    // Create collectin for and user Informations
+    const foodCollection = client.db('RestaurantDB').collection('Food');
+
+
+     //jwt
+     app.post("/jwt", async (req, res) => {
+      const userEmail = req.body
+      // console.log(userEmail)
+      const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN, {
+        expiresIn: '10d'
+      })
+      res.send({ token })
+    })
+
+
     // get for all data from mongodb
-    app.get('/all-foods', async(req, res) =>{
-        const cursor = infoCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/all-foods', async (req, res) => {
+      const cursor = infoCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // For update operation
+    app.get('/foods/:id', async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await foodCollection.findOne(query);
+      res.send(result);
+    })
+
+    // For Update operation
+    app.put('/foods/:id', async(req,res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const options = {upsert: true};
+      const UpdatedFoods = req.body;
+      
+      const Foods = {
+        $set:{
+          foodname:UpdatedFoods.foodname,
+          quantity:UpdatedFoods.quantity,
+          price:UpdatedFoods.price, 
+          buyername:UpdatedFoods.buyername, 
+          buyeremail:UpdatedFoods.buyeremail, 
+          buyingdate:UpdatedFoods.buyingdate, 
+          photoURL:UpdatedFoods.photoURL,
+        }
+      }
+      const result = await foodCollection.updateOne(query, Foods, options);
+      res.send(result)
     })
 
     // find data id wise for show details
-    app.get('/all-foods/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id)};
+    app.get('/all-foods/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
 
-        const options = {
-          // Include only the `title` and `imdb` fields in each returned document
-            projection: {  name: 1, image: 1, category: 1,  quantity: 1, madeBy: 1, foodOrig :1, description: 1 },
-          };
-      
-
-        const result = await infoCollection.findOne(query);
-        res.send(result);
+      const options = {
+        // Include only the `title` and `imdb` fields in each returned document
+        projection: { name: 1, image: 1, category: 1, quantity: 1, madeBy: 1, foodOrig: 1, description: 1 },
+      };
+      const result = await infoCollection.findOne(query);
+      res.send(result);
     })
 
-    // For Parchase 
-    
+    // For read user data from mongodb
+    app.get('/foods', async(req,res) => {
+      const cursor = foodCollection.find();
+      const result = await cursor.toArray();
+      res.send(result); 
+  })
+
+
+    // using post operation for post food items on mongodb
+    app.post('/foods', async (req, res) => {
+      const addFoodItems = req.body;
+      console.log(addFoodItems);
+      const result = await foodCollection.insertOne(addFoodItems)
+      res.send(result);
+    })
+
+    // Delete foods items 
+    app.delete('/foods/:id', async (req,res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await foodCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -66,13 +146,10 @@ async function run() {
 run().catch(console.dir);
 
 
-
-
-
 app.get('/', (req, res) => {
-    res.send('Art and craft server is running')
+  res.send('Art and craft server is running')
 })
 
 app.listen(port, () => {
-    console.log(`Art and Craft Server is running on port: ${port}`)
+  console.log(`Art and Craft Server is running on port: ${port}`)
 })
